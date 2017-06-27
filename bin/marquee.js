48a354965fbc5b0ad26938d6f2ba8c69a15db65f
Cameron Samuels <satchmosanders@gmail.com>
2017-06-26 20:28:38 -0500

    Removed state because of anothe approach

--- bin/marquee.js
+++ bin/marquee.js
@@ -1,13 +1,11 @@
 var MarqueeTitle = {};
 MarqueeTitle.chars = []; //define global variable
-MarqueeTitle.sate = true;
 MarqueeTitle.start = function(title, speed, seperator) {
 	title += " " + seperator + " ";
 	MarqueeTitle.chars = title.split('');
 	MarqueeTitle.interval = setInterval(MarqueeTitle.cycle, speed);
 };
 MarqueeTitle.cycle = function() {
-	if (!MarqueeTitle.state) return;
 	MarqueeTitle.chars.push(MarqueeTitle.chars[0]);
 	MarqueeTitle.chars.shift();
 	document.title = MarqueeTitle.chars.join("");
