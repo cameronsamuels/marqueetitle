4ca8fb698cc9db8a5ff1ac406b2d69c3098a3de1
Cameron Samuels <satchmosanders@gmail.com>
2017-06-27 16:32:25 -0500

    Added MarqueeTitle.shifts property
    This allows multiple shifts every interval instead of only being able to manually shift multiple.

--- bin/marquee.js
+++ bin/marquee.js
@@ -1,5 +1,6 @@
 /* MarqueeTitle v2.0pre | MIT License | git.io/vQZbs */
 var MarqueeTitle = {}; MarqueeTitle.chars = [], MarqueeTitle.title = "", MarqueeTitle.speed = 250, MarqueeTitle.direction = 0,
+MarqueeTitle.shifts = 1,
 MarqueeTitle.start = function(title, speed, seperator) {
 	title = (title || MarqueeTitle.title || document.title) + " " + (seperator||" ") + " ";
 	MarqueeTitle.chars = title.split('');
@@ -23,7 +24,7 @@ MarqueeTitle.loop = function(times, title, speed, seperator) {
 MarqueeTitle.pause = function() { clearInterval(MarqueeTitle.interval) },
 MarqueeTitle.resume = function() { MarqueeTitle.interval = setInterval(MarqueeTitle.cycle, MarqueeTitle.speed) },
 MarqueeTitle.cycle = function(times) {
-	times = times || 1;
+	times = times || MarqueeTitle.shifts || 1;
 	do {
 		if (MarqueeTitle.direction) MarqueeTitle.chars.unshift(MarqueeTitle.chars[MarqueeTitle.chars.length-1]), MarqueeTitle.chars.pop();
 		else MarqueeTitle.chars.push(MarqueeTitle.chars[0]), MarqueeTitle.chars.shift();	
