0253f6c8b06b4b316d361763b0561559e6924cd4
Cameron Samuels <satchmosanders@gmail.com>
2017-06-26 20:24:03 -0500

    Removed comments and blak lines

--- bin/marquee.js
+++ bin/marquee.js
@@ -1,25 +1,10 @@
 var MarqueeTitle = {};
 MarqueeTitle.chars = []; //define global variable
-
-/*=====
-Call the MarqueeTitle.start() function to start the animation
-Paramaters:
-	-title = 	String, text on the title
-	-speed = 	Integer, speed in milleseconds
-					of the interval to cal the
-					cycle function
-	-seperator  String, the divider between the
-	                beginning and end
-=====*/
-
 MarqueeTitle.start = function(title, speed, seperator) {
 	title += " " + seperator + " ";
 	MarqueeTitle.chars = title.split('');
 	setInterval(MarqueeTitle.cycle, speed);
 };
-
-/* This function is called every (speed) milliseconds.
-	This makes the title marquee */
 MarqueeTitle.cycle = function() {
 	MarqueeTitle.chars.push(MarqueeTitle.chars[0]);
 	MarqueeTitle.chars.shift();
