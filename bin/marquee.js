3627a9581a858637141f6464b2b081c640de54cd
Cameron Samuels <satchmosanders@gmail.com>
2017-06-27 08:38:42 -0500

    Changed MarqueeTitle.speed to 250 instead of 0

--- bin/marquee.js
+++ bin/marquee.js
@@ -1,6 +1,6 @@
 /* MarqueeTitle v2.0pre | MIT License | git.io/vQZbs */
 var MarqueeTitle = {};
-MarqueeTitle.chars = [], MarqueeTitle.title = "", MarqueeTitle.speed = 0,
+MarqueeTitle.chars = [], MarqueeTitle.title = "", MarqueeTitle.speed = 250,
 MarqueeTitle.start = function(title, speed, seperator) {
 	title = (title || MarqueeTitle.title || document.title) + " " + (seperator||" ") + " ";
 	MarqueeTitle.chars = title.split('');
