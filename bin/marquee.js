7c5845ee5af7c8c00f4eff6f44d6f1bd82f2732e
Cameron Samuels <satchmosanders@gmail.com>
2017-06-26 20:40:13 -0500

    Changed semi-colons to commas

--- bin/marquee.js
+++ bin/marquee.js
@@ -1,13 +1,13 @@
 var MarqueeTitle = {};
-MarqueeTitle.chars = [];
+MarqueeTitle.chars = [],
 MarqueeTitle.start = function(title, speed, seperator) {
 	title += " " + seperator + " ";
 	MarqueeTitle.chars = title.split('');
 	MarqueeTitle.interval = setInterval(MarqueeTitle.cycle, speed);
-};
-MarqueeTitle.stop = function() { clearInterval(MarqueeTitle.interval) }
+},
+MarqueeTitle.stop = function() { clearInterval(MarqueeTitle.interval) },
 MarqueeTitle.cycle = function() {
 	MarqueeTitle.chars.push(MarqueeTitle.chars[0]);
 	MarqueeTitle.chars.shift();
 	document.title = MarqueeTitle.chars.join("");
-};
+}
