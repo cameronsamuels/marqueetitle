8ae78b18321baea08d4c0ec1cad58245430bec2d
Cameron Samuels <satchmosanders@gmail.com>
2017-06-28 14:41:45 -0500

    Fixed syntax & added game.update() to example app

--- examples/click-counter/script.js
+++ examples/click-counter/script.js
@@ -1,4 +1,6 @@
-var game = { on:true.time:10 }; //initial variables
+var game = {
+   on: true, time: 10, score: 0, button: document.querySelector('button')
+}; //initial variables
 game.pause = function(){
    game.on = false; //pause the game
    MarqueeTitle.pause(); //pause the marquee animation
@@ -6,6 +8,9 @@ game.pause = function(){
 game.resume = function() {
   game.on = true; //resume the game
   MarqueeTitle.resume(); //resume the marquee animation
+},
+game.update = function() {
+   button.textContent = game.score; //update the button text to show the score
 },
 game.over = function() {
   alert('Game over: you earned ' + game.score + 'points!'); //notifiy for the game to be over
@@ -15,10 +20,11 @@ game.refresh = function() {
   if (game.on == false) return; //stop execution if paused
   game.time--; //subtract 1 from time left
   if (game.time == 0) game.over(); //call game over if ran out of time
+  game.update(); //update the UI
 }
 game.click = function() {
   if (game.on == false) return; //stop execution if paused
   game.score++; //add 1 to score
 },
 game.interval = setInterval(game.refresh, 1000); //set repeating to every 1 second to call game.refresh()
-document.querySelector('button').addEventListener('click', game.click); //add click listeners to earn points
+button.addEventListener('click', game.click); //add click listeners to earn points
