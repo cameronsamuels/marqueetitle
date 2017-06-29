4cddf0065d6b0ec5413aac286d35e00320c0293c
Cameron Samuels <satchmosanders@gmail.com>
2017-06-29 11:15:34 -0500

    Cleaned up marquee.slim.js

--- bin/marquee.slim.js
+++ bin/marquee.slim.js
@@ -1,7 +1,7 @@
 /* MarqueeTitle Slim v2.0pre | MIT License | git.io/vQZbs */
 var mqt = {
   start : function(a, b) {
-    mqt.d = ((a || document.title) + " " + (b||"") + " ").split('');
-    setInterval(function(){mqt.d.push(mqt.d[0]), mqt.d.shift();document.title = mqt.d.join("")}, 250);
+    mqt.d = ((a || document.title) + (b||"  ")).split('');
+    setInterval(function(){mqt.d.push(mqt.d[0]),mqt.d.shift();document.title=mqt.d.join("")},250)
   }
 }
