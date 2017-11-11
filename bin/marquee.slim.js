aee08760369cb39e814943977b7510fc3b10cafb
Cameron Samuels <me@cameronsamuels.com>
2017-11-10 21:37:48 -0600

    (improvement)Updated comments in mqt
    - Changed incorrect license claim
    - Shortened the instructions comment

--- bin/marquee.slim.js
+++ bin/marquee.slim.js
@@ -1,8 +1,8 @@
-/* MarqueeTitle Slim v2.0pre | MIT License | git.io/vQZbs */
+/* MarqueeTitle Slim v2.0pre | Cameron Samuels License | git.io/vQZbs */
 var mqt = {
   start : function(a, b) {
     mqt.d = ((a || document.title) + (b||"  ")).split('');
     setInterval(function(){mqt.d.push(mqt.d[0]),mqt.d.shift();document.title=mqt.d.join("")},250)
   }
 }
-//mqt.start(); //uncomment this line if you want the marquee to auto start, or use this line somewhere else to start it
+//mqt.start(); //use this function to start the marquee
