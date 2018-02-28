c3c3d1c8db06eeaf446340ad0965a0b0635d8ffd
Cameron Samuels <me@cameronsamuels.com>
2018-02-27 20:32:22 -0600

    (issue)Fixed the click counter example (#42)
    - Fixed the click counter from not working when starting over
    - Cleaned up a lot of code to make it minimalist
    - Removed green button styles over 50 clicks
    - More to clean up
    
    This resolves issue #34.

--- examples/click-counter/README.md
+++ examples/click-counter/README.md
@@ -1,7 +1,7 @@
 # Click Counter
-This application demonstrates practical use of [MarqueeTitle.pause()](https://git.io/vFVpC), [MarqueeTitle.resume()](https://git.io/vFVpn), and [MarqueeTitle.start()](https://git.io/vFVpZ).
+This application demonstrates practical use of [MarqueeTitle.pause()](https://git.io/vFVpC) and [MarqueeTitle.start()](https://git.io/vFVpZ).
 
 # Get Started
-Go to <https://cameronsamuels.com/marqueetitle/examples/click-counter> to interact with the example.
+Go to <https://git.io/vAPsg> to interact with the example.
 
-Also, you can view the documentation of the methods used in this example including [MarqueeTitle.pause()](https://git.io/vFVpC), [MarqueeTitle.resume()](https://git.io/vFVpn), and [MarqueeTitle.start()](https://git.io/vFVpZ).
+You can view the documentation of the methods used in this example in the [Wiki](https://git.io/vFVR4).
--- examples/click-counter/script.js
+++ examples/click-counter/script.js
@@ -1,43 +1,27 @@
-var game = {
-  on: false, time: 10, score: 0, finished: true,
-  button: document.querySelector('button'), timeText: document.querySelector('h2')
-}; //initial variables
-game.pause = function(){
-  game.on = false; //pause the game
-  MarqueeTitle.pause(); //pause the marquee animation
-},
-game.resume = function() {
-  game.on = true; //resume the game
-  MarqueeTitle.resume(); //resume the marquee animation
-},
-game.update = function() {
-  if (game.on == false) return; //stop executing if game is not running
-  game.button.innerHTML = game.score; //update the button text to show the score
-  game.timeText.textContent = game.time; //update the time text to show the time left
-},
-game.over = function() {
-  game.pause(); //pause the game
-  game.finished = true; //set variable to be over
-  game.timeText.textContent = "Times up!!"; //notifiy that the game is over
-  game.button.style.background = game.score > 50 ? "#0d0" : "#d00"; //set the button's backgroud to a user-friendly color
-  game.button.style.border = "10px solid " + (game.score > 50 ? "#0a0" : "#a00"); //set the button's border to a user-friendly color
-},
-game.refresh = function() {
-  if (game.on == false) return; //stop execution if paused
-  game.time--; //subtract 1 from time left
-  if (game.time == 0) game.over(); //call game over if ran out of time
-  game.update(); //update the UI
-},
-game.start = function() {
-  MarqueeTitle.start(); //start the marquee tab title animation
-  game.on = true; //enable game refresh
-  game.finished = false; //set game to be not over
-  game.interval = setInterval(game.refresh, 1000); //set repeating to every 1 second to call game.refresh()
-},
-game.click = function() {
-  if (game.finished == true) game.start(); //start game if game is over
-  if (game.on == false) return; //stop execution if paused
-  game.score++; //add 1 to score
-  game.update(); //update the text of the button instantly before the interval executes
-},
-game.button.addEventListener('click', game.click); //add click listeners to earn points
+var time, score, running, loop;
+var button = document.querySelector("button");
+var text = document.querySelector("h2");
+
+button.addEventListener("click", function() {
+  if (!running) {
+    time = 10;
+    score = 0;
+    button.style.background = "";
+    button.style.borderColor = "";
+    text.textContent = "10";
+    running = true;
+    MarqueeTitle.start(); //animate tab title
+    loop = setInterval(function() {
+      text.textContent = --time > 0 ? time : "Times up!!";
+      if (!time) {
+        clearInterval(loop);
+        running = "cooldown";
+        setTimeout(function(){ running = false }, 2000);
+        MarqueeTitle.pause(); //stop tab title animation
+        button.style.background = "#d00";
+        button.style.borderColor = "#a00";
+      }
+    }, 1000);
+  }
+  if (running == true) button.textContent = ++score;
+});
