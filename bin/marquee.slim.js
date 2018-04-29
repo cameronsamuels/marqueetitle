7ebcf0e5b0c71175aee1ea937d5048b9b1b75406
Cameron Samuels <me@cameronsamuels.com>
2018-04-29 12:55:59 -0500

    (update)Removed mqt slim

--- bin/marquee.slim.js
+++
@@ -1,8 +0,0 @@
-/* MarqueeTitle Slim v2.0pre | Cameron Samuels License | git.io/vQZbs */
-var mqt = {
-  start : function(a, b) {
-    mqt.d = ((a || document.title) + (b||"  ")).split('');
-    setInterval(function(){mqt.d.push(mqt.d[0]),mqt.d.shift();document.title=mqt.d.join("")},250)
-  }
-}
-//mqt.start(); //use this function to start the marquee
