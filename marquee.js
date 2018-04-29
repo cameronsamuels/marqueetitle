e141f530e793eefbb61aca5684fbf3a55005bde9
Cameron Samuels <me@cameronsamuels.com>
2018-04-29 12:57:54 -0500

    (code)Updated the library code
    - Moved the code to the root folder
    - Added parameter descriptions

--- marquee.js
+++ marquee.js
@@ -1,4 +1,6 @@
 /* MarqueeTitle v3.0 | Cameron Samuels License | git.io/vQZbs */
+
+// c = text, a = text separator, m = scroll speed
 function MarqueeTitle(c, a, m) {
 	window.title = (c || document.title) + " " + (a || " ") + " ";
 	setInterval(function() {
