2db2d97ca45a0bd4ddb07b5991bb2888d8b25252
Cameron Samuels <me@cameronsamuels.com>
2018-06-01 15:40:05 -0500

    (update)v4.0 - Major improvements

--- marquee.js
+++ marquee.js
@@ -1,13 +1,12 @@
-/* MarqueeTitle v3.0 | Cameron Samuels License | git.io/vQZbs */
-
-// c = text, a = text separator, m = scroll speed
-function MarqueeTitle(c, a, m) {
-	window.title = (c || document.title) + " " + (a || " ") + " ";
-	setInterval(function() {
-		var x = window.title.split("");
-		x.push(x[0]);
-		x.shift();
-		window.title = x.join("");
-		document.title = window.title;
-	}, m || 300);
-}
+/* MarqueeTitle v4.0 | Cameron Samuels License | git.io/vQZbs */
+(function(c, a, m) {
+  var title = (c || document.title) + " " + (a || "-") + " ";
+  setInterval(function() {
+    title = title.substring(1) + title.charAt(0);
+    document.title = title;
+  }, m || 300);
+})(
+  /* Tab Text */ "This webpage has a scrolling tab title",
+  /* Title Repeat Separator */ "-",
+  /* Scroll Speed (in milleseconds) */ 300
+);
